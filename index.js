c45fb03839daf7d0666063a349bab1076bb8184a
javier <javia506@gmail.com>
2022-12-11 16:40:38 -0800

    commit tree 921b92cba58c56452ad69eda6fecd92b5264e646

--- index.js
+++ index.js
@@ -1,1 +1,134 @@
-// Write your solution here!
+const cats =[] 
+{
+cats.push("Milo");
+cats.push("Otis");
+cats.push("Garfield");
+
+
+}
+
+function destructivelyAppendCat(name)
+
+{
+cats.push("Ralph");
+
+}
+
+function destructivelyPrependCat(name){
+cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+cats.shift();
+
+}
+
+function appendCat(name){
+const newArray=cats.slice();
+newArray.push("Broom");
+return newArray;
+}
+
+function prependCat(name){
+    const newArray= ["Arnold",... cats];
+    return newArray;
+    
+}
+
+function removeLastCat(name){
+    const newArray=cats.slice();
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat(name){
+    const newArray=cats.slice();
+    newArray.shift();
+    return newArray;
+    
+}
+
+describe('index.js', function () {
+    describe('cats', function () {
+      it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
+        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+  
+    describe('Array functions', function () {
+      beforeEach(function () {
+        cats.length = 0;
+  
+        cats.push('Milo', 'Otis', 'Garfield');
+      });
+  
+      describe('destructivelyAppendCat(name)', function () {
+        it('appends a cat to the end of the cats array', function () {
+          destructivelyAppendCat('Ralph');
+  
+          expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
+        });
+      });
+  
+      describe('destructivelyPrependCat(name)', function () {
+        it('prepends a cat to the beginning of the cats array', function () {
+          destructivelyPrependCat("Bob");
+  
+          expect(cats).to.have.ordered.members(["Bob", "Milo", "Otis", "Garfield"]);
+        });
+      });
+  
+      describe('destructivelyRemoveLastCat()', function () {
+        it('removes the last cat from the cats array', function () {
+          destructivelyRemoveLastCat();
+  
+          expect(cats).to.have.ordered.members(["Milo", "Otis"]).and.to.not.include('Garfield');
+        });
+      });
+  
+      describe('destructivelyRemoveFirstCat()', function () {
+        it('removes the first cat from the cats array', function () {
+          destructivelyRemoveFirstCat();
+  
+          expect(cats).to.have.ordered.members(["Otis", "Garfield"]).and.to.not.include('Milo');
+        });
+      });
+  
+      describe('appendCat(name)', function () {
+        it('appends a cat to the cats array and returns a new array, leaving the cats array unchanged', function () {
+          expect(appendCat("Broom")).to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
+  
+          expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+        });
+      });
+  
+      describe('prependCat(name)', function () {
+        it('prepends a cat to the cats array and returns a new array, leaving the cats array unchanged', function () {
+          expect(prependCat("Arnold")).to.have.ordered.members(["Arnold", "Milo", "Otis", "Garfield"]);
+  
+          expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+        });
+      });
+  
+      describe('removeLastCat()', function () {
+        it('removes the last cat in the cats array and returns a new array, leaving the cats array unchanged', function () {
+          expect(removeLastCat()).to.have.ordered.members(["Milo", "Otis"]);
+  
+          expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+        });
+      });
+  
+      describe('removeFirstCat()', function () {
+        it('removes the first cat from the cats array and returns a new array, leaving the cats array unchanged', function () {
+          expect(removeFirstCat()).to.have.ordered.members(["Otis", "Garfield"]);
+  
+          expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+        });
+      });
+    });
+  });
+  
